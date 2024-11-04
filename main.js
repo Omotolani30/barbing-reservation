87e25ea4792105ec8cdbd54461d5a0c38bab2cf1
Omotolani Afolayan <afolayanomotolani30@gmail.com>
2024-11-04 18:07:02 +0100

    commits

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-// Select the DOM elements
+// toggle
 const toggleButton = document.getElementById('toggle');
 const navLinks = document.getElementById('nav-links');
 
-// Add an event listener to toggle the 'active' class on click
 toggleButton.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
 
+// members
 // Function to open modal
 function openModal(id) {
     document.getElementById(id).style.display = "flex";
@@ -18,27 +18,7 @@ function closeModal(id) {
     document.getElementById(id).style.display = "none";
 }
 
-// document.querySelectorAll('.team-member').forEach(member => {
-//     member.addEventListener('click', function() {
-//         const memberId = this.dataset.id;
-//         // Fetch data from backend (this part interacts with backend)
-//         fetch(`/team-member/${memberId}`)
-//             .then(response => response.json())
-//             .then(data => {
-//                 // Populate popup with team member info
-//                 document.getElementById('popup-name').innerText = data.name;
-//                 document.getElementById('popup-role').innerText = data.role;
-//                 document.getElementById('popup-bio').innerText = data.bio;
-//                 // Display popup
-//                 document.getElementById('team-popup').style.display = 'block';
-//             });
-//     });
-// });
 
-
-
-// Example of dynamic availability based on barber selection
 document.getElementById('barber').addEventListener('change', function() {
     var selectedBarber = this.value;
-    // You can dynamically update available services or time slots based on the barber selected
 });
